051b64909ca18216640edfcf41bf776add803770
Mabu-1 <umahtab65@gmail.com>
2026-08-18 21:30:36 +0600

    fix: singleton wix client with cookie session

--- components/ProductPage/ProductInfo.js
+++ components/ProductPage/ProductInfo.js
@@ -43,7 +43,7 @@ export default function ProductInfo({
   const handleShopNow = async () => {
     setCheckingOut(true);
     try {
-      await addToCart(productId, selectedOptions, 1);
+      await addToCart(productId, 1);
       await checkout();
     } catch (err) {
       console.error("Checkout error:", err);
