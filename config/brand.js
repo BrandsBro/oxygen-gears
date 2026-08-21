3faffb2f21ee50e08264386889a6d61e968af37c
Mabu-1 <umahtab65@gmail.com>
2026-08-21 14:58:25 +0600

    fix: update logo URL

--- config/brand.js
+++ config/brand.js
@@ -1,6 +1,6 @@
 const brand = {
   name: "OxygenGears",
-  logo: "https://static.wixstatic.com/media/20cc08_535dcd70e52b4cd985d792139d87dd9e~mv2.avif",
+  logo: "https://static.wixstatic.com/media/8f1bc7_d7500b59abbf4ac988b49d4c57d60764~mv2.webp",
   favicon: "https://static.wixstatic.com/media/20cc08_36a5efa395264583a64742a8c3f8ae06~mv2.png",
   tagline: "Reclaim Your Freedom With Portable Oxygen Concentrator",
   email: "hello@oxygengears.com",
