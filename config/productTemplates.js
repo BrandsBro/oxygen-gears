1fbf2b43bd48a0ade0110e1a3e4744e0a5b51e53
Mabu-1 <umahtab65@gmail.com>
2026-08-31 19:05:40 +0600

    fix: update all product slugs to match new Wix store

--- config/productTemplates.js
+++ config/productTemplates.js
@@ -10,27 +10,27 @@ const productTemplates = {
     config: () => import("@/config/products/rechargeable-1-5l-portable-oxygen-concentrator-95-oxygen-purity"),
   },
 
-  "rechargeable-battery-for-1-7lmin-portable-oxygen-concentrator": {
+  "rechargeable-battery-for-1-7l-oxygen-concentrator": {
     template: "accessory",
     config: () => import("@/config/products/rechargeable-battery-for-1-7lmin-portable-oxygen-concentrator"),
   },
 
-  "5-pieces-nasal-cannulas-for-1-7lmin-portable-oxygen-concentrator-2m-flexible": {
+  "5-pieces-nasal-cannulas-for-1-7l": {
     template: "accessory",
     config: () => import("@/config/products/5-pieces-nasal-cannulas-for-1-7lmin-portable-oxygen-concentrator-2m-flexible"),
   },
 
-  "reliable-home-charging-adapter-for-1-7lmin-portable-oxygen-concentrator": {
+  "charging-adapter-for-1-7l-oxygen-concentrator": {
     template: "accessory",
     config: () => import("@/config/products/reliable-home-charging-adapter-for-1-7lmin-portable-oxygen-concentrator"),
   },
 
-  "car-charging-cable-for-1-7lmin-portable-oxygen-concentrator": {
+  "car-charging-cable-for-portable-oxygen-concentrator": {
     template: "accessory",
     config: () => import("@/config/products/car-charging-cable-for-1-7lmin-portable-oxygen-concentrator"),
   },
 
-  "shoulder-strap-carry-bag-for-1-7lmin-portable-oxygen-concentrator": {
+  "carry-bag-for-1-7l-oxygen-concentrator": {
     template: "accessory",
     config: () => import("@/config/products/shoulder-strap-carry-bag-for-1-7lmin-portable-oxygen-concentrator"),
   },
